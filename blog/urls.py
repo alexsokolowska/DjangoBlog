b51e06e0d6ceaea5c105475aeadb8b6cd530dcd6
alexsokolowska <alexs@physik.uzh.ch>
2017-05-07 11:39:57 +0200

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #beginning^ followed by end $ will match (empty)
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 #post_list is the name of the Unified Resource Locator that will be used to identify the view
